3901f7eab3af53ea4d23a3e7c2ef5c3e1ff6e5c9
williammabotjadev <williammabotjadev@gmail.com>
2022-05-07 22:21:04 +0200

    home icon, positioning

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from kivymd.uix.screen import MDScreen
 from kivymd.app import MDApp
-from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.button import MDRaisedButton, MDIconButton
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.banner import MDBanner
 from kivymd.uix.toolbar import MDToolbar
+from kivy.lang import Builder
+from kivymd.uix.card import MDCard
+
 
 class Example(MDApp):
     data = {
@@ -13,18 +16,23 @@ class Example(MDApp):
 
     def build(self):
         screen = MDScreen()
-        pos_count = 0.5
+        pos_count = 0.4
         top_bar = MDToolbar()
         top_bar.title = 'Welcome to CasaCRM'
         top_bar.pos_hint = { 'top': 1 }
         top_bar.md_bg_color = (.32, .42, .68, 1)
         screen.add_widget(top_bar)
+        icon_btn = MDIconButton()
+        icon_btn.pos_hint = { 'center_x': 0.5, 'center_y': 0.8 }
+        icon_btn.type = 'image'
+        icon_btn.icon = 'images/landing.png'
+        screen.add_widget(icon_btn)
         for text in self.data:
             custom_btn = MDRaisedButton()
             custom_btn.text = str(text)
             custom_btn.pos_hint = {'center_x': 0.5, 'center_y': pos_count}
             custom_btn.md_bg_color = (.32, .42, .68, 1)
-            pos_count += 0.1
+            pos_count -= 0.1
             screen.add_widget(custom_btn)
         return screen
 
